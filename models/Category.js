d0ed74078e613c2cbee7a60555c3983179cbd08d
cupcakesprinkle3 <79061264+cupcakesprinkle3@users.noreply.github.com>
2021-06-21 19:00:37 -0700

    worked on product and category models and routes

--- models/Category.js
+++ models/Category.js
@@ -5,9 +5,27 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
+{
+  id: {
+    // use the special Sequelize DataTypes object provide what type of data it is
+    type: DataTypes.INTEGER,
+    // this is the equivalent of SQL's `NOT NULL` option
+    allowNull: false,
+    // instruct that this is the Primary Key
+    primaryKey: true,
+    // turn on auto increment
+    autoIncrement: true
   },
+  // define a username column
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      // this means the string must be at least 1 char long
+      len: [1]
+    }
+  }
+},
   {
     sequelize,
     timestamps: false,
--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,59 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define a username column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the string must be at least 1 char long
+        len: [1]
+      }
+    }
+  },
+  {
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    }
+  },
+  {
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    }
+  },
+  {
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -18,6 +65,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+
 );
 
 module.exports = Product;
